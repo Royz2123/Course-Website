a38ba5b67263b612a2af68d7c9ce6734fda4181c
DaniRubin <37410225+DaniRubin@users.noreply.github.com>
2018-06-08 00:45:16 +0300

    Solution to the change client view

--- Client side/js/maz.js
+++ Client side/js/maz.js
@@ -22,16 +22,31 @@ function multi_toggle_visibility(){
 	for (i = 0; i < tags_to_change.length; i++) { 
 		toggle_visibility(tags_to_change[i]);
 	}
+}
 	
 	
+function first_toggle_visibility(){
+	var tags_to_change = ["mainPart", "dataPart"];
+	
+	for (i = 0; i < tags_to_change.length; i++) { 
+		toggle_visibility(tags_to_change[i]);
+	}
+}
 
 function toggle_visibility(id) {
+  console.log(id);
   var e = document.getElementById(id);
-  if (e.style.display == 'block')
+  if (e.style.display == 'inline-block')
 	 e.style.display = 'none';
   else
-	 e.style.display = 'block';}
+	 e.style.display = 'inline-block';
+}
+  
 
+
+ 
+ 
+ 
 function toggle_editor(html){
   document.getElementById(left_part_edit_html) = html;
 }
