e43f8232a266bd52c2ae98c15e7539628f111d27
Royz2123 <royzohar25@gmail.com>
2018-06-13 11:08:16 +0300

    DONT TOUCH DANIIII

--- fileserver.js
+++ fileserver.js
@@ -4,70 +4,95 @@ const fs = require('fs');
 const path = require('path');
 const port = process.argv[2] || 9000;
 
+
+
+
 http.createServer(function (req, res) {
   console.log(`${req.method} ${req.url}`);
 
   // parse URL
   const parsedUrl = url.parse(req.url);
-   
-
-  let pathname = "";
-  if(parsedUrl.pathname == '/getfile')
+ 
+  // extract query parameters
+  var parms = null;
+  if (parsedUrl.query != null) 
   {
-      parms = parsedUrl.query.split('&')
-      pathname = parms[0].split('=')[1] 
-       
-  }else {
-      pathname = `.${parsedUrl.pathname}`;
+      parms = parsedUrl.query.split('&')   
   }
   
-  console.log(pathname);
-  
-  // extract URL path
-  
-  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-  const ext = path.parse(pathname).ext;
-  // maps file extention to MIME typere
-  const map = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword'
-  };
-
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      // if the file is not found, return 404
-      res.statusCode = 404;
-      res.end(`File ${pathname} not found!`);
+  // handle app services
+  let pathname = "";
+  if (parsedUrl.pathname == '/register') 
+  {
+      console.log("Tried to sign up" + parms)
+      res.statusCode = 200;
+      res.end("Registering coming soon");
       return;
-    }
-
-    // if is a directory search for index file matching the extention
-    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
-
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', map[ext] || 'text/plain' );
-        res.end(data);
+  }
+  else if (parsedUrl.pathname == '/login') 
+  {
+      console.log("Tried to login" + parms)  
+      res.statusCode = 200;
+      res.end("Login coming soon");
+      return;      
+  }
+  
+  // this case means the user want a file
+  else{
+      if(parsedUrl.pathname == '/getfile')
+      {
+          pathname = parms[0].split('=')[1];
+      }else 
+      {
+          pathname = `.${parsedUrl.pathname}`;
       }
-    });
-  });
 
+      console.log(pathname);
 
+      // extract URL path
+      
+      // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+      const ext = path.parse(pathname).ext;
+      // maps file extention to MIME typere
+      const map = {
+        '.ico': 'image/x-icon',
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.json': 'application/json',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.wav': 'audio/wav',
+        '.mp3': 'audio/mpeg',
+        '.svg': 'image/svg+xml',
+        '.pdf': 'application/pdf',
+        '.doc': 'application/msword'
+      };
+
+      fs.exists(pathname, function (exist) {
+        if(!exist) {
+          // if the file is not found, return 404
+          res.statusCode = 404;
+          res.end(`File ${pathname} not found!`);
+          return;
+        }
+
+        // if is a directory search for index file matching the extention
+        if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+        // read file from file system
+        fs.readFile(pathname, function(err, data){
+          if(err){
+            res.statusCode = 500;
+            res.end(`Error getting the file: ${err}.`);
+          } else {
+            // if the file is found, set Content-type and send data
+            res.setHeader('Content-type', map[ext] || 'text/plain' );
+            res.end(data);
+          }
+        });
+      });
+   }
 }).listen(parseInt(port));
 
 console.log(`Server listening on port ${port}`);
