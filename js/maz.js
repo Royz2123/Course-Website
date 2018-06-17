453ceb74cd12d499ce0932948d2f4481d758e79e
DaniRubin <37410225+DaniRubin@users.noreply.github.com>
2018-06-17 14:39:49 +0300

    Changes in the htmls

--- js/maz.js
+++ js/maz.js
@@ -49,3 +49,32 @@ function toggle_editor(html){
 function toggle_viewer(html){
   document.getElementById("right_part_show_html").innerHTML = html;
 }
+
+
+
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function openLogin() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+
+/*window.onclick = function(event) {
+
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+*/
