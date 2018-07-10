6315115a8b264bd9f5c91277d34f2669dc2df9af
DaniRubin <37410225+DaniRubin@users.noreply.github.com>
2018-07-10 16:03:24 +0300

    All the changes 3.0

---
+++ js/dani.js
@@ -0,0 +1,14 @@
+
+
+function daniTheKingNotForMazAllOnMyDickArachArachArach(){	
+		  document.getElementById("login").style.display = 'none';
+	      document.getElementById("signUp").style.display = 'none';
+	      document.getElementById("logout").style.display = 'inline-block';	
+}
+
+function daniTheKingNotForMazAllOnMyDickArachArachArach2(){
+		  document.getElementById("login").style.display = 'inline-block';
+	      document.getElementById("signUp").style.display = 'inline-block';
+	      document.getElementById("logout").style.display = 'none';	
+	
+}
